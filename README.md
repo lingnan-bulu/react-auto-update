3be4bf4010d911876e16fd9175927ef01483394f
buxiangjian <buxj@bjyn.com>
2018-05-30 16:56:40 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# react-auto-mount
-this is a method for react's optimization
+
+## Before
+in component
+```js
+
+class xxxx extends React.Component {
+  ...
+  shouldComponentUpdate(){
+    if(...){
+      return true or false
+    }
+  }
+}
+```
+
+## After
+Component only with local imports
+```js
+import reactAutoUpdate from 'react-auto-update'
+@reactAutoUpdate
+class xxxx extends React.Component {
+  ...
+}
+```
+
+## Installation 
+1. Install
+```npm install react-auto-update --save```
+
+2. Add react-auto-update in component top:
+```js
+@reactAutoUpdate
+
+but this package depend on immutable.js
+Done!
---
+++ index.js
@@ -0,0 +1,28 @@
+import { is } from 'immutable'
+export default function optimizeMount (baseClass) {
+	return class extends baseClass {
+		constructor(props){
+			super(props)
+		}
+		shouldComponentUpdate(nextProps = {}, nextState = {}){
+			const thisProps = this.props || {}, thisState = this.state || {};
+			if (Object.keys(thisProps).length !== Object.keys(nextProps).length ||
+			    Object.keys(thisState).length !== Object.keys(nextState).length) {
+			  return true;
+			}
+
+			for (const key in nextProps) {
+			  if (thisProps[key] !== nextProps[key] || !is(thisProps[key], nextProps[key])) {
+			    return true;
+			  }
+			}
+
+			for (const key in nextState) {
+			  if (thisState[key] !== nextState[key] || !is(thisState[key], nextState[key])) {
+			    return true;
+			  }
+			}
+			return false;
+		}
+	}
+}
